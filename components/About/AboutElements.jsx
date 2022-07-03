a73e3f50060c70cb611bcab2acbfe1b9121c3820
Oroghene Emudainohwo <oemudainohwo@gmail.com>
2022-07-03 08:38:34 -0400

    about page

---
+++ components/About/AboutElements.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const AboutContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`;
+
+export const TreeContainer = stlyed.div`
+  
+`;
